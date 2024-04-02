82cd8473037890367792bb5ec7947628be636b1a
Papoulas, Konstantin <konstantin.papoulas@kumavision.com>
2024-04-02 15:22:35 +0200

    feat: Get-AzureDevOpsPullRequestWorkItems

--- DevOps/Get-AzureDevOpsPullRequestWorkItems.ps1
+++ DevOps/Get-AzureDevOpsPullRequestWorkItems.ps1
@@ -7,19 +7,19 @@ function Get-AzureDevOpsPullRequestWorkItems {
         [Parameter(Mandatory=$true)]
         [string] $AzureDevOpsToken
     )
-    $results = @()
+    $result = @()
     foreach ($pullRequest in $AzureDevOpsPullRequest) {        
         $uri = Get-AzureDevOpsUri -AzureDevOpsProject $AzureDevOpsProject -Route "_apis/git/repositories/$($pullRequest.repository)/pullRequests/$($pullRequest.pullRequestId)/workitems"         
         $response = Invoke-AzureDevOpsWebRequest -Uri $uri -Method GET -AzureDevOpsToken $AzureDevOpsToken        
         $jWorkItems = $response.Content | ConvertFrom-Json
         foreach ($workItem in $jWorkItems.value) {
-            $uri = "https://dev.azure.com/$script:devopsOrganisation/$script:devopsProject/_apis/wit/workitems/$($workItem.Id)?`$expand=relations&api-version=6.0"    
+            $uri = Get-AzureDevOpsUri -AzureDevOpsProject $AzureDevOpsProject -Route "_apis/wit/workitems/$($workItem.Id)" -Parameter '$expand=relations&api-version=6.0'            
             $response = Invoke-AzureDevOpsWebRequest -Uri $uri -Method GET -AzureDevOpsToken $AzureDevOpsToken                 
-            $jWorkItem = $response.Content | ConvertFrom-Json  
-            $jWorkItem | Add-Member -MemberType NoteProperty -Name pullRequest -Value $pullRequest        
-            $results += $jWorkItem 
+            $workItem = $response.Content | ConvertFrom-Json  
+            $workItem | Add-Member -MemberType NoteProperty -Name pullRequest -Value $pullRequest        
+            $result += $workItem 
         }
     }
-    return $results
+    return $result
 }
 Export-ModuleMember -Function Get-AzureDevOpsPullRequestWorkItems
