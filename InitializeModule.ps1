26af2872498fb34be932febcb5477eb583c77417
Papoulas, Konstantin <konstantin.papoulas@kumavision.com>
2024-04-03 09:19:43 +0200

    refactor: module version

--- InitializeModule.ps1
+++ InitializeModule.ps1
@@ -1,4 +1,5 @@
-$Global:Config = Get-Content "PSDevelopmentSupport.config.json" | ConvertFrom-Json
+$configPath = Join-Path $PSScriptRoot "PSDevelopmentSupport.config.json"
+$Global:Config = Get-Content $configPath | ConvertFrom-Json
 $Global:DevSuiteBearerToken = [System.Environment]::GetEnvironmentVariable($Global:Config.EnvVarDevSuiteBearerToken, "User") 
 $Global:JiraApiToken = [System.Environment]::GetEnvironmentVariable($Global:Config.EnvVarJiraApiToken, 'User')
 if (-not $Global:JiraApiToken){
